8ca16638fc44fb6c311f94eec790aca5218a3a94
danghoski <danghoskinfotech@gmail.com>
2021-06-22 17:26:52 +0100

    initial new commit

---
+++ app/src/main/java/com/aghogho/todoapp/HomeActivity.kt
@@ -0,0 +1,20 @@
+package com.aghogho.todoapp
+
+import android.icu.number.NumberFormatter
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class HomeActivity : AppCompatActivity() {
+
+    private lateinit var usernameDisplay: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        usernameDisplay = findViewById(R.id.userNameInput)
+
+        usernameDisplay.text = intent.getStringExtra("Name")
+    }
+}
